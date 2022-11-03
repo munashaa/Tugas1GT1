a0e9fabb1e59a9f0617fd2f08130e5e3fbeb9154
Fin121 <raehanabdillah6@gmail.com>
2022-11-03 15:27:22 +0700

    Modifying the GUI so it looks better

--- GUI.py
+++ GUI.py
@@ -1,50 +1,38 @@
 from tkinter import *
 window = Tk()
 window.title("kasir GUI di Python")
-window.geometry('350x200')
-lbl = Label(window, text="Masukan Nilai Pertama : ",anchor="e",width=20)
-lbl.grid(column=0, row=0)
+window.minsize(width=500, height=700)
+window.configure(bg="green")
+
+title_name = Label(text="Hits Cafe",font=("Times New Roman",20,"bold"),bg="green")
+title_name.grid(column=1,row=0)
+
+title_name2 = Label(text="List Menu:",font=("Times New Roman",15,"bold"),bg="green")
+title_name2.grid(column=0,row=1)
+
+lbl = Label(text="Americano (20k)\t: ",anchor="e",font=("Times New Roman",15,"normal"),bg="green")
+lbl.place(x=10, y=70)
+lbl = Spinbox(from_=0, to=10, width=5)
+lbl.place(x=200, y=75)
  
-nilai1 = Entry(window,width=10)
-nilai1.grid(column=1,row=0)
+lbl2 = Label(text="Mocha Latte (15k)\t: ",anchor="e",font=("Times New Roman",15,"normal"),bg="green")
+lbl2.place(x=10, y=95)
+lbl2 = Spinbox(from_=0, to=10, width=5)
+lbl2.place(x=200, y=100)
  
- 
-lbl2 = Label(window, text="Masukan Nilai Kedua : ",anchor="e",width=20)
-lbl2.grid(column=0, row=1)
- 
-nilai2 = Entry(window,width=10)
-nilai2.grid(column=1,row=1)
- 
-lbl3 = Label(window, text="Hasil : ",anchor="e",width=20)
-lbl3.grid(column=0, row=2)
- 
-hasil = Label(window, text="0",anchor="w",width=10)
-hasil.grid(column=1, row=2)
- 
-def tambah():
-    hasil.configure(text=(int(nilai1.get())+int(nilai2.get())))
- 
-def kurang():
-    hasil.configure(text=(int(nilai1.get())-int(nilai2.get())))
- 
-def kali():
-    hasil.configure(text=(int(nilai1.get())*int(nilai2.get())))
- 
-def bagi():
-    hasil.configure(text=(int(nilai1.get())/int(nilai2.get())))
- 
- 
-btn = Button(window, text="Tambah", command=tambah)
-btn.grid(column=0, row=3)
- 
-btn = Button(window, text="Kurang", command=kurang)
-btn.grid(column=1, row=3)
- 
-btn = Button(window, text="Kali", command=kali)
-btn.grid(column=0, row=4)
- 
-btn = Button(window, text="Bagi", command=bagi)
-btn.grid(column=1, row=4)
- 
+lbl3 = Label(text="Cappuccino (17k)\t: ",anchor="e",font=("Times New Roman",15,"normal"),bg="green")
+lbl3.place(x=10, y=120)
+lbl3 = Spinbox(from_=0, to=10, width=5)
+lbl3.place(x=200, y=125)
+
+lbl4 = Label(text="Ice Tea (8k)\t: ",anchor="e",font=("Times New Roman",15,"normal"),bg="green")
+lbl4.place(x=10, y=145)
+lbl4 = Spinbox(from_=0, to=10, width=5)
+lbl4.place(x=200, y=150)
+
+lbl2 = Label(text="Mineral Water (5k)\t: ",anchor="e",font=("Times New Roman",15,"normal"),bg="green")
+lbl2.place(x=10, y=170)
+lbl2 = Spinbox(from_=0, to=10, width=5)
+lbl2.place(x=200, y=175)
 
 window.mainloop()
